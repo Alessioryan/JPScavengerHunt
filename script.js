2e54e737cb1940207dd9c19fe5b2986cf6b63671
Alessioryan <alessio.ryan@gmail.com>
2024-07-19 12:18:38 +0200

    Commit styles and add more secure password recon

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function checkPassword() {
     const input = document.getElementById('passwordInput').value;
-    if (input === 'password') {
-        alert('Success');
+    const hashedInput = CryptoJS.SHA256(input).toString();
+    const correctHash = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'; 
+    if (hashedInput === correctHash) {
+        alert("You shouldn't be here yet... but you guessed well! Come back when the website is done.");
     } else {
-        alert('Incorrect password');
+        alert('Incorrect!');
     }
 }
